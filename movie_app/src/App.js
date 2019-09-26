d8fc954c8430c8f65b1525465c6e078a1be809cf
DaeHun <skypedanny@naver.com>
2019-09-26 18:09:51 +0900

    Data flow with Props

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -1,21 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const App = () => (
-  <div className="App">
-      <Movie />
-    </div>
-)
+const movieTitles = [
+  "JohnWick1",
+  "JohnWick2",
+  "JohnWick3",
+  "Fast & Furious"
+]
 
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <Movie />
-//       </div>
-//     )
-//   }
-// }
+//이미지 주소 긁어오기
+const movieImages = [
+  "https://upload.wikimedia.org/wikipedia/en/thumb/9/98/John_Wick_TeaserPoster.jpg/220px-John_Wick_TeaserPoster.jpg",
+  "https://img01.mgo-images.com/image/thumbnail/v2/content/MMV9D183EB36CDF6353B4D01D15A3BF1F70A.jpeg",
+  "https://images-na.ssl-images-amazon.com/images/I/91wc7yc2R8L._SL1500_.jpg",
+  "https://www.dvdsreleasedates.com/posters/800/F/Fast-and-Furious-Presents-Hobbs-and-Shaw-2019-movie-poster.jpg"
+]
+
+//메인 컴포넌트가 데이터를 다 갖고 있음
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Movie title={movieTitles[0]} poster={movieImages[0]} />
+        <Movie title={movieTitles[1]} poster={movieImages[1]} />
+        <Movie title={movieTitles[2]} poster={movieImages[2]} />
+        <Movie title={movieTitles[3]} poster={movieImages[3]} />
+    </div>
+    );
+  }
+}
 
 export default App;
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,24 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Movie.css';
 
-const Movie = () => (
-    <div>
-        <MoviePoster />
-        <h1>Hello! This is a Moive</h1>
-    </div>
-)
+class Movie extends Component{
+    render() {
+        return(
+            <div>
+                <MoviePoster poster={this.props.poster} />
+                <h1>{ this.props.title }</h1>
+            </div>
+        )
+    }
+}
 
-const MoviePoster = () => (
-    <img src="https://img.hankyung.com/photo/201908/01.20268354.1.jpg" alt="빡빡이" />
-)
-
-
-// class Movie extends Component{
-//     render() {
-//         return(
-//             <h1>Hello! This is a movie</h1>
-//         )
-//     }
-// }
+class MoviePoster extends Component{
+    render() {
+        return(
+            <img src={this.props.poster} alt="빡빡이들"/>
+        )
+    }
+}
+    
+    
 
 export default Movie;
