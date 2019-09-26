e7582387482f625969ad1e76998bca5bc0f7db63
DaeHun <skypedanny@naver.com>
2019-09-26 22:49:17 +0900

    Validating Props with Prop

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -4,18 +4,22 @@ import Movie from './Movie';
 
 const movies =[
   {
+    id: 1,
     title: "JohnWick1",
     poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/98/John_Wick_TeaserPoster.jpg/220px-John_Wick_TeaserPoster.jpg",
   },
   {
+    id: 2,
     title: "JohnWick2",
     poster: "https://img01.mgo-images.com/image/thumbnail/v2/content/MMV9D183EB36CDF6353B4D01D15A3BF1F70A.jpeg"
   },
   {
+    id: 3,
     title: "JohnWick3",
     poster: "https://images-na.ssl-images-amazon.com/images/I/91wc7yc2R8L._SL1500_.jpg"
   },
   {
+    id: 4,
     title: "Fast & Furious",
     poster: "https://www.dvdsreleasedates.com/posters/800/F/Fast-and-Furious-Presents-Hobbs-and-Shaw-2019-movie-poster.jpg"
   }
@@ -27,8 +31,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        {movies.map(movie => {
-          return <Movie title={movie.title} poster={movie.poster} />
+        {movies.map((movie, index) => {
+          return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
     );
--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component{
+
+    static propTypes = {
+        title: PropTypes.string,
+        poster: PropTypes.string
+    }
+
     render() {
         return(
             <div>
@@ -12,7 +19,13 @@ class Movie extends Component{
     }
 }
 
+//필수요건으로 지정후 데이터가 없을경우 메세지 받음
 class MoviePoster extends Component{
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render() {
         return(
             <img src={this.props.poster} alt="빡빡이들"/>
