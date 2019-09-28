f8c2cfe7926d5e830e5511f4b08143926d50c727
DaeHun <skypedanny@naver.com>
2019-09-28 23:13:01 +0900

    Async, Await
    1.fetch를 callApi로 변경
    2.get movies(async function)function 사용
    3.callApi는 fetch promise를 return할 것인데 이는 모든 데이터의 json임
    4.컴포넌트의 state를 movies로 set
    5.state안에 movies가 있으면 render movies라는 function 불러옴
    6.render movies는 movies불러오고 이는 맵핑을 함(title, poster)

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -17,18 +17,30 @@ class App extends Component {
 //AjAX는 url을 JavaScript로 비동기화로 불러온다.
 //Ajax의 A가 Asynchronous인 이유
   componentDidMount() {
-    fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
-    .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err)) 
+     this._getMovies();
   }
 
   //리액트 자체 함수와 나의 함수에 차이를 두기위해 _를 사용함
   _renderMovies = () => {
     const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id} />
     })
     return movies
+  }
+
+  //callApi의 return value를 movies에 set
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.lt/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
   }
 
   render() {
