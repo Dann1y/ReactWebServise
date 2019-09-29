a01df6c036902956ee2f13a645b1fdb14ebf1494
DaeHun <skypedanny@naver.com>
2019-09-29 14:07:04 +0900

    Updates

---
+++ precache-manifest.3fd7cdc71b9f3d2db91cf9861b69b646.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a570ad36c2cf99155c053e8faf07e1cd",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "e18e13dee4a0ba888711",
+    "url": "/movie_app/static/css/main.ee9ee63f.chunk.css"
+  },
+  {
+    "revision": "d23de20d4a05f64dfc7d",
+    "url": "/movie_app/static/js/2.cdbf9396.chunk.js"
+  },
+  {
+    "revision": "e18e13dee4a0ba888711",
+    "url": "/movie_app/static/js/main.14deaf92.chunk.js"
+  },
+  {
+    "revision": "a3a26237de1de1d250ab",
+    "url": "/movie_app/static/js/runtime-main.ebbd9dda.js"
+  }
+]);
